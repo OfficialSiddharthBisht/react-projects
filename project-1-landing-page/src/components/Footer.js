aa3aac27587732303f6e152da35637406992715a
Siddharth <officialsiddharthbisht@gmail.com>
2023-03-28 16:15:28 +0530

    feat: footer complete

---
+++ project-1-landing-page/src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <div>
+            <h1>Landing Page</h1>
+            <p>@all rights reserved Siddharth Bisht 2022</p>
+        </div>
+        <div>
+            <h5>Connect With Us</h5>
+            <div>
+                <a href="https://github.com/OfficialSiddharthBisht" target={'blank'}>GitHub</a>
+                <a href="https://siddharthbisht.netlify.app/" target={'blank'}>Website</a>
+                <a href="https://www.linkedin.com/in/siddharth-bisht-57084b198/" target={'blank'}>LinkedIn</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
