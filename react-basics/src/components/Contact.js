77935ef3a789032e686bbd5a77f706a101ebe168
Siddharth <officialsiddharthbisht@gmail.com>
2023-04-06 06:35:00 +0530

    feat: revised useState hook

--- react-basics/src/components/Contact.js
+++ react-basics/src/components/Contact.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Contact = () => {
+  let name = "initial"; 
+  /* this name value is changing but there is no data binding i.e changing the value in the program will change the value in console but there will be no change 
+    in the value inside tage like p h1 etc for that we need a hook called useState
+  */
+ const [email , setEmail] = useState("email");
+ /*
+  now with the help of use state there will be data binding and with the change of the value of the variable there will be a total change in its value both in 
+  console and jsx tags like p h1 etc
+ */
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <form onSubmit={(e)=>{e.preventDefault()}}>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => (name = e.target.value)}
+        />
+        <input 
+          type="email" 
+          placeholder="Email" 
+          onChange={(e)=>{setEmail(e.target.value)}}
+          />
+        <button onClick={()=> console.log(name,email)}>Click </button>
+        <p>{name}</p>
+        <p>{email}</p>
+      </form>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
